80279984a8e74b0a6a3af892e6858ce32a77f418
Stephen McKinney <steve@wojodesign.com>
2015-10-29 11:03:21 -0500

    closes #9

--- generators/app/templates/_gulpfile.js/tasks/build-production.js
+++ generators/app/templates/_gulpfile.js/tasks/build-production.js
@@ -3,5 +3,5 @@ var gulpSequence = require('gulp-sequence');
 
 gulp.task('build:production', function(cb) {
   process.env.NODE_ENV = 'production';
-  gulpSequence('clean', ['fonts', 'images', 'misc'], ['sass', 'html'], cb);
+  gulpSequence('clean', ['fonts', 'images', 'misc'], ['sass', 'html', 'webpack:production'], cb);
 });
